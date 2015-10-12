a3dafcc68b049ecf14f1b875aec5e53cf5415d46
Ross Kaffenberger <rosskaff@gmail.com>
2015-10-12 14:54:07 -0400

    Refactor google authentication to work with new google-api-client

--- lib/lionel/google_authentication.rb
+++ lib/lionel/google_authentication.rb
@@ -1,55 +1,74 @@
+require "google/api_client"
+
 module Lionel
   class GoogleAuthentication
     include Configurable
 
-    attr_accessor :access_token
+    attr_accessor :access_token, :refresh_token
     attr_writer :client
     config_accessor :google_client_id, :google_client_secret
 
     def data
       raise "No access token" unless access_token
       {
-        google_token: access_token.token,
-        google_refresh_token: access_token.refresh_token,
+        google_token: access_token,
+        google_refresh_token: refresh_token,
         google_client_id: google_client_id,
         google_client_secret: google_client_secret
       }
     end
 
     def retrieve_access_token(authorization_code)
-      @access_token = client.auth_code.get_token(authorization_code,
-                                                 :redirect_uri => "urn:ietf:wg:oauth:2.0:oob")
+      authorization.code = authorization_code
+      authorization.fetch_access_token!
+      @access_token = authorization.access_token
+      @refresh_token = authorization.refresh_token
     end
 
     def refresh
-      return false unless refresh_token
+      return false unless refresh_token && access_token
 
-      current_token = OAuth2::AccessToken.from_hash(client,
-                                                    :refresh_token => refresh_token,
-                                                    :expires_in => one_year
-                                                   )
-      @access_token = current_token.refresh! # returns new access_token
+      authorization.access_token = access_token
+      authorization.refresh_token = refresh_token
+      authorization.refresh!
+
+      @access_token = authorization.access_token
     end
 
     def authorize_url
-      client.auth_code.authorize_url(
-        :redirect_uri => "urn:ietf:wg:oauth:2.0:oob",
-        :scope => scopes.join(' ')
-      )
+      authorization.authorization_uri
     end
 
     def api_console_url
       "https://code.google.com/apis/console"
     end
 
+    def authorization
+      @authorization ||= begin
+                           auth = client.authorization
+                           auth.client_id = google_client_id
+                           auth.client_secret = google_client_secret
+                           auth.scope = scopes
+                           auth.expires_in = one_year
+                           auth.redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+                           auth
+                         end
+    end
+
     def client
-      @client ||= OAuth2::Client.new(google_client_id, google_client_secret,
-                                     :site => "https://accounts.google.com",
-                                     :token_url => "/o/oauth2/token",
-                                     :authorize_url => "/o/oauth2/auth")
+      @client ||= Google::APIClient.new(auto_refresh_token: true)
     end
 
     private
+
+    def build_client
+      auth = client.authorization
+      auth.client_id = google_client_id
+      auth.client_secret = google_client_secret
+      auth.scope = scopes
+      auth.redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+      client
+    end
 
     def refresh_token
       @refresh_token || configuration.google_refresh_token
@@ -64,7 +83,7 @@ module Lionel
     end
 
     def one_year # in seconds
-      31557600
+      60*60*24*36
     end
 
   end
